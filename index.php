d6c67bc3635a140be75883671b7cd9b8787c2066
Joemir J. Santos <joemir.jsantos@gmail.com>
2018-04-28 21:34:03 -0300

    tela de Login

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 // namespaces
 use \Slim\Slim;
-use Hcode\Page;
-use Hcode\PageAdmin;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -27,11 +28,42 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
     
+    User::verifyLogin();
+
 	$page = new PageAdmin(); // vai chamar o construtct
 
 	$page->setTpl("index"); // apos esta linha vai chamar o destruct da classe Page
 
 });
+
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]); // vai chamar o construtct
+
+	$page->setTpl("login"); // apos esta linha vai chamar o destruct da classe Page
+
+});
+
+$app->post('/admin/login', function(){
+   
+ 	User::login($_POST["login"], $_POST["password"]);
+ 	
+ 	header("Location: /admin"); //redireciona a para esta pagina
+ 	exit;
+
+});
+
+$app->get('/admin/logout', function(){
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+});
+
 
 $app->run();
 
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,6 +9,8 @@ class Page{
 	private $tpl;
 	private $options;
 	private $defaults = [
+		"header"=>true,
+		"footer"=>true,
 		"data"=>[]
 	];
 
@@ -32,7 +34,9 @@ class Page{
 
 		$this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if($this->options["header"] === true)
+			$this->tpl->draw("header");
+
 
 	}
 
@@ -54,6 +58,7 @@ class Page{
 
 	public function __destruct(){
 
+		if($this->options["footer"] === true)
 		$this->tpl->draw("footer");
 
 
